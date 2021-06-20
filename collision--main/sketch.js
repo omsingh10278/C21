a3de7e4d004cb3f5678b8efc1ae241b8be9edb41
omsingh10278 <85404480+omsingh10278@users.noreply.github.com>
2021-06-20 21:02:43 +0530

    Add files via upload

---
+++ collision--main/sketch.js
@@ -0,0 +1,43 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  car = createSprite(300, 200,80,30);
+  car.velocityX = 5
+  car.shapeColor = "aqua";
+
+  wall = createSprite(1000, 100, 25, 1600);
+  wall.shapeColor = "grey";
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  
+  else {
+    movingRect.shapeColor = "green";
+  
+    fixedRect.shapeColor = "green"
+  }
+ 
+  bounceOff(car,wall)
+  drawSprites();
+}
+
